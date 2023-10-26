88cf43d529719e294d80bdae8aa22616d6beb623
Diego Vásquez <dvasquezar@gmail.com>
2023-10-26 16:12:53 -0600

    Add client data seeder preferences

--- app/src/main/java/db/seeders/ClientDataSeeder.kt
+++ app/src/main/java/db/seeders/ClientDataSeeder.kt
@@ -7,48 +7,72 @@ import db.helpers.ClientDbHelper
 
 class ClientDataSeeder(private val context: Context) {
 
+    private val preferences = context.getSharedPreferences("seed_preferences", Context.MODE_PRIVATE)
+
     fun seed() {
         val dbHelper = ClientDbHelper(context)
         val db = dbHelper.writableDatabase
 
-        val clients = listOf(
-            ContentValues().apply {
-                put(ClientContract.ClientEntry.COLUMN_NAME_NAME, "Diego Vásquez")
-                put(ClientContract.ClientEntry.COLUMN_NAME_EMAIL, "diego@vs.com")
-                put(ClientContract.ClientEntry.COLUMN_NAME_PASSWORD, "\$2a\$10\$.qoOlpKfFwMC7pRg9/XXau/FLhFFzPWq/iKoRZbScKONnr8L4b3R2")
-            },
-            ContentValues().apply {
-                put(ClientContract.ClientEntry.COLUMN_NAME_NAME, "José Bardales")
-                put(ClientContract.ClientEntry.COLUMN_NAME_EMAIL, "jose@vs.com")
-                put(ClientContract.ClientEntry.COLUMN_NAME_PASSWORD, "\$2a\$10\$.qoOlpKfFwMC7pRg9/XXau/FLhFFzPWq/iKoRZbScKONnr8L4b3R2")
-            },
-            ContentValues().apply {
-                put(ClientContract.ClientEntry.COLUMN_NAME_NAME, "Fernanda Martínez")
-                put(ClientContract.ClientEntry.COLUMN_NAME_EMAIL, "fernanda@vs.com")
-                put(ClientContract.ClientEntry.COLUMN_NAME_PASSWORD, "\$2a\$10\$.qoOlpKfFwMC7pRg9/XXau/FLhFFzPWq/iKoRZbScKONnr8L4b3R2")
-            },
-            ContentValues().apply {
-                put(ClientContract.ClientEntry.COLUMN_NAME_NAME, "Jairo Soto")
-                put(ClientContract.ClientEntry.COLUMN_NAME_EMAIL, "jairo@vs.com")
-                put(ClientContract.ClientEntry.COLUMN_NAME_PASSWORD, "\$2a\$10\$.qoOlpKfFwMC7pRg9/XXau/FLhFFzPWq/iKoRZbScKONnr8L4b3R2")
-            },
-            ContentValues().apply {
-                put(ClientContract.ClientEntry.COLUMN_NAME_NAME, "María Martínez")
-                put(ClientContract.ClientEntry.COLUMN_NAME_EMAIL, "maria@vs.com")
-                put(ClientContract.ClientEntry.COLUMN_NAME_PASSWORD, "\$2a\$10\$.qoOlpKfFwMC7pRg9/XXau/FLhFFzPWq/iKoRZbScKONnr8L4b3R2")
-            },
-        )
+        val alreadySeeded = preferences.getBoolean("client_data_already_seeded", false)
 
-        db.beginTransaction()
-        try {
-            for (client in clients) {
-                db.insert(ClientContract.ClientEntry.TABLE_NAME, null, client)
+        if (!alreadySeeded) {
+
+            val clients = listOf(
+                ContentValues().apply {
+                    put(ClientContract.ClientEntry.COLUMN_NAME_NAME, "Diego Vásquez")
+                    put(ClientContract.ClientEntry.COLUMN_NAME_EMAIL, "diego@vs.com")
+                    put(
+                        ClientContract.ClientEntry.COLUMN_NAME_PASSWORD,
+                        "\$2a\$10\$.qoOlpKfFwMC7pRg9/XXau/FLhFFzPWq/iKoRZbScKONnr8L4b3R2"
+                    )
+                },
+                ContentValues().apply {
+                    put(ClientContract.ClientEntry.COLUMN_NAME_NAME, "José Bardales")
+                    put(ClientContract.ClientEntry.COLUMN_NAME_EMAIL, "jose@vs.com")
+                    put(
+                        ClientContract.ClientEntry.COLUMN_NAME_PASSWORD,
+                        "\$2a\$10\$.qoOlpKfFwMC7pRg9/XXau/FLhFFzPWq/iKoRZbScKONnr8L4b3R2"
+                    )
+                },
+                ContentValues().apply {
+                    put(ClientContract.ClientEntry.COLUMN_NAME_NAME, "Fernanda Martínez")
+                    put(ClientContract.ClientEntry.COLUMN_NAME_EMAIL, "fernanda@vs.com")
+                    put(
+                        ClientContract.ClientEntry.COLUMN_NAME_PASSWORD,
+                        "\$2a\$10\$.qoOlpKfFwMC7pRg9/XXau/FLhFFzPWq/iKoRZbScKONnr8L4b3R2"
+                    )
+                },
+                ContentValues().apply {
+                    put(ClientContract.ClientEntry.COLUMN_NAME_NAME, "Jairo Soto")
+                    put(ClientContract.ClientEntry.COLUMN_NAME_EMAIL, "jairo@vs.com")
+                    put(
+                        ClientContract.ClientEntry.COLUMN_NAME_PASSWORD,
+                        "\$2a\$10\$.qoOlpKfFwMC7pRg9/XXau/FLhFFzPWq/iKoRZbScKONnr8L4b3R2"
+                    )
+                },
+                ContentValues().apply {
+                    put(ClientContract.ClientEntry.COLUMN_NAME_NAME, "María Martínez")
+                    put(ClientContract.ClientEntry.COLUMN_NAME_EMAIL, "maria@vs.com")
+                    put(
+                        ClientContract.ClientEntry.COLUMN_NAME_PASSWORD,
+                        "\$2a\$10\$.qoOlpKfFwMC7pRg9/XXau/FLhFFzPWq/iKoRZbScKONnr8L4b3R2"
+                    )
+                },
+            )
+
+            db.beginTransaction()
+            try {
+                for (client in clients) {
+                    db.insert(ClientContract.ClientEntry.TABLE_NAME, null, client)
+                }
+                db.setTransactionSuccessful()
+            } finally {
+                db.endTransaction()
             }
-            db.setTransactionSuccessful()
-        } finally {
-            db.endTransaction()
-        }
 
-        dbHelper.close()
+            dbHelper.close()
+
+            preferences.edit().putBoolean("client_data_already_seeded", true).apply()
+        }
     }
 }
